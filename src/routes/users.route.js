95c695d256f573974c8788ff6104eee594e813d5
David Carrasco <davidack123456789@gmail.com>
2026-02-13 22:34:16 -0500

    Creando peticiones http

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,6 +1,24 @@
 import { Router }  from "express";
 const router = Router()
+
 router.get('/users', (req,res) =>{
+    res.send('obteniendo usuarios')
+})
+
+router.get('/users/:id', (req,res) =>{
+    const {id} = req.params
+    res.send('obteniendo unicos usuarios' + id)
+})
+
+router.post('/users', (req,res) =>{
+    res.send('creando  usuario')
+})
+
+router.delete('/users/:id', (req,res) =>{
+    res.send('eliminando usuarios')
+})
+
+router.put('/users/:id', (req,res) =>{
     res.send('obteniendo usuarios')
 })
 
